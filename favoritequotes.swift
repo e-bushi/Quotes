922ebdac82658d71f9d2bd5a3e023e121aed80ad
e-武士 <chris@aluna.io>
2023-06-11 16:19:04 -0400

    Initial commit for favorite quotes assignment ```

---
+++ favoritequotes.swift
@@ -0,0 +1,16 @@
+/**
+    * Created by Kuza on 6/11/2023.
+    * This file is part of the Favorite Quotes Exercise file set.
+    * (c) 2023 Kuza <
+
+    Favorite Quotes Exercise
+
+    This file contains quotes inserted by all the students that have been apart of the Become A Programmer Course.
+    This is the first as assignment as a programmer. The goal is to get familiar with Source Control.
+
+    If you are a student, add your favorite quote to the list below, with your name and the date you added it.
+    I'll start with mine.
+**/
+
+// Kuza's Favorite Quote - 6/11/2023
+print("'All things change in a dynamic environment. Your effort to remain what you are is what limits you.' - Motoko Kusanagi, Ghost in the Shell")
